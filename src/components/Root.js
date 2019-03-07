dcad25681899fa0b2b3ffc5250875780d713a14f
endorphin82 <endorphin82@gmail.com>
2019-03-07 17:10:37 +0200

    003-sign_in_form_and_redux-form

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,12 +1,18 @@
 import React, { Component } from "react";
+import { Route } from "react-router-dom";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
+
+import AdminPage from "./routes/AdminPage";
+import AuthPage from "./routes/AuthPage";
 
 class Root extends Component {
   render() {
     return (
       <div>
-        <h1>Root</h1>
+        <Route path="/admin" component={AdminPage} />
+        <Route path="/auth" component={AuthPage} />
+
       </div>
     );
   }
--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 import { routerReducer } from "react-router-redux";
+import {reducer as form} from "redux-form";
 import Reducers from "./tmpreducer";
 
 export default combineReducers({
   router: routerReducer,
+  form,
   ...Reducers
-})
+});
 
