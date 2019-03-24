b0c96576ee3b68c7439c0c9b08fc58cfb83d277b
endorphin82 <endorphin82@gmail.com>
2019-03-24 20:44:12 +0200

    029-react-motion_animate_add_and_remove_events_from_selectedvents

--- src/components/events/SelectedEvents.js
+++ src/components/events/SelectedEvents.js
@@ -2,17 +2,48 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { selectedEventsSelector } from "../../ducks/events";
 import SelectedEventCard from "./SelectedEventCard";
+import { TransitionMotion, spring, presets } from "react-motion";
 
 class SelectedEvents extends Component {
+  getStyles() {
+    return this.props.events.map(event => ({
+      style: {
+        opacity: spring(1, {
+          damping: presets.noWobble.damping,
+          stiffness: presets.noWobble.stiffness / 10
+        })
+      },
+      key: event.uid,
+      data: event
+    }));
+  }
+
+  willLeave = () => ({
+    opacity: spring(0)
+  });
+
+  willEnter = () => ({
+    opacity: 0
+  });
+
   render() {
-    return (
-      <div>
-        {this.props.events.map(event => <SelectedEventCard event={event} key={event.uid}/>)}
+    return <TransitionMotion
+      styles={this.getStyles()}
+      willLeave={this.willLeave}
+      willEnter={this.willEnter}
+    >
+      {(interpolated) => <div>
+        {
+          interpolated.map(config => <div style={config.style} key={config.key}>
+            <SelectedEventCard event={config.data}/>
+          </div>)
+        }
       </div>
-    );
+      }
+    </TransitionMotion>;
   }
 }
 
 export default connect(state => ({
   events: selectedEventsSelector(state)
-}))(SelectedEvents);
+}), null, null, { pure: false })(SelectedEvents);
